704b0787ce46bdfde84fcc5691c4c46e0ce0fd9f
LSamKill <sfelipepto01@gmail.com>
2026-04-29 23:07:54 -0500

    favoritos actu

--- js/favoritos.js
+++ js/favoritos.js
@@ -1,14 +1,11 @@
-// js/favoritos.js
 function agregarFavorito(id) {
-    // Obtenemos favoritos o creamos un array vacío si no existe
     let favoritos = JSON.parse(localStorage.getItem("favoritos")) || [];
-
-    // Verificamos si ya existe para no duplicar
+    
     if (!favoritos.includes(id)) {
         favoritos.push(id);
         localStorage.setItem("favoritos", JSON.stringify(favoritos));
-        alert("Agregado a favoritos ❤️");
+        alert("Añadido a tus favoritos ❤️");
     } else {
-        alert("Este servicio ya está en tus favoritos");
+        alert("Este servicio ya está en tu lista.");
     }
 }
